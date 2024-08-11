d30142355e5e403524ae6caca3642608fe4e5781
Pakonchai <Prakonchai2546@gmail.com>
2024-08-11 21:03:03 +0700

    NewConnecPage

---
+++ app/src/main/java/com/example/reviewhub/Register_Create_PasswordActivity.kt
@@ -0,0 +1,20 @@
+package com.example.reviewhub
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Register_Create_PasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_create_password)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
