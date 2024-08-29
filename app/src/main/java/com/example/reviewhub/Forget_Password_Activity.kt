6a3f88f2b3c901dab7491ece1700d8f46c79345c
Earth <earthsrichok31@gmail.com>
2024-08-29 15:53:02 +0700

    compleate sprint 1

--- app/src/main/java/com/example/reviewhub/Forget_Password_Activity.kt
+++ app/src/main/java/com/example/reviewhub/Forget_Password_Activity.kt
@@ -31,11 +31,6 @@ class Forget_Password_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forget_password)
-
-
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -49,7 +44,7 @@ class Forget_Password_Activity : AppCompatActivity() {
 
 
         SendOTPbutton.setOnClickListener {
-            LodingDialog.show()
+//            LodingDialog.show()
             val email = emailEditText.text.toString()
 //            Handler(Looper.getMainLooper()).postDelayed({
 //                LodingDialog.cancel()
